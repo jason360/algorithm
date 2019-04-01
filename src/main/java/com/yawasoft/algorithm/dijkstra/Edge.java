96bf2d54401565409ecd753a973f3ede49df4c13
jason360 <jason.yd.wu@gmail.com>
2019-03-31 23:49:10 -0400

    adds Dijkstra implementation

---
+++ src/main/java/com/yawasoft/algorithm/dijkstra/Edge.java
@@ -0,0 +1,58 @@
+package com.yawasoft.algorithm.dijkstra;
+
+import java.util.Objects;
+
+public class Edge implements Comparable<Edge>{
+    private Node start;
+    private Node end;
+    private int weight;
+
+    public Edge(Node start, Node end, int weight) {
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+
+    public Node getStart() {
+        return start;
+    }
+
+    public void setStart(Node start) {
+        this.start = start;
+    }
+
+    public Node getEnd() {
+        return end;
+    }
+
+    public void setEnd(Node end) {
+        this.end = end;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Edge edge = (Edge) o;
+        return Objects.equals(start, edge.start) &&
+                Objects.equals(end, edge.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end);
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.weight - o.weight;
+    }
+}
